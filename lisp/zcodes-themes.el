4ca206e3087e39bc40c7e4338b07f7278674c9a8
zcodes <zcodes@163.com>
2016-01-29 13:28:30 +0800

    add theme change function

--- lisp/zcodes-themes.el
+++ lisp/zcodes-themes.el
@@ -1,5 +1,17 @@
 (el-get-bundle zcodes/emacs-material-theme
   (add-to-list 'custom-theme-load-path default-directory)
-  (load-theme 'material-light t))
+  (load-theme 'material t))
+
+(defun zcodes/use-light-theme ()
+  (interactive)
+  (load-theme 'material-light t)
+  (force-mode-line-update)
+  (powerline-reset))
+
+(defun zcodes/use-dark-theme ()
+  (interactive)
+  (load-theme 'material t)
+  (force-mode-line-update)
+  (powerline-reset))
 
 (provide 'zcodes-themes)
