361847b39f6ede3d19166bfd22cb0a714ce7a8b6
zcodes <zcodes@163.com>
2016-02-23 12:45:26 +0800

    add copyright headers.

--- lisp/zcodes-company.el
+++ lisp/zcodes-company.el
@@ -1,4 +1,19 @@
+;;; zcodes-company.el --- Emacs company.
+;;
+;; Copyright (c) 2016 zcodes
+;;
+;; Author: zcodes <zcodes@qq.com>
+;; URL: https://github.com/zcodes/emacs.d
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+
 (el-get-bundle company-mode)
 (add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'zcodes-company)
+
+;;; zcodes-company.el ends here.
