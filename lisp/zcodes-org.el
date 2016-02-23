16ca64b3456dab6dce8a74ff5e1c3ff511f3fd08
zcodes <zcodes@163.com>
2016-02-23 20:33:57 +0800

    修复因Emacs在Terminal下设置org-mode字体出错

--- lisp/zcodes-org.el
+++ lisp/zcodes-org.el
@@ -17,7 +17,7 @@
 
 ;; (el-get-bundle elpa:org)
 
-;; keybinds for org-mode 
+;; keybinds for org-mode
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
@@ -26,15 +26,16 @@
 (setq org-log-done t)
 
 ;; 单独设置org-table的字体, 保证有中文是表格对齐
-(setq fontset-orgtable (create-fontset-from-ascii-font "Fira Mono Medium-14"))
-(set-fontset-font fontset-orgtable 'han
-		  (font-spec :family "思源黑体 Medium"
-			     :size 16.5))
+(when (window-system)
+  (setq fontset-orgtable (create-fontset-from-ascii-font "Fira Mono Medium-14"))
+  (set-fontset-font fontset-orgtable 'han
+		    (font-spec :family "思源黑体 Medium"
+			       :size 16.5))
 
-(defun zcodes/org-table-font ()
-  (set-face-attribute 'org-table nil
-		      :fontset fontset-orgtable))
-(add-hook 'org-mode-hook 'zcodes/org-table-font)
+  (defun zcodes/org-table-font ()
+    (set-face-attribute 'org-table nil
+			:fontset fontset-orgtable))
+  (add-hook 'org-mode-hook 'zcodes/org-table-font))
 
 ;;  org-export
 (setq org-html-doctype "html5")
