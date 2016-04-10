9977f31f70d8ba855fc3b35de4a43e826889657c
zcodes <zcodes@163.com>
2016-04-10 22:14:54 +0800

    make neotree behaving like vim's.

--- lisp/zcodes-neotree.el
+++ lisp/zcodes-neotree.el
@@ -12,8 +12,11 @@
 
 (el-get-bundle emacs-neotree)
 
+;; just use ascii characters for display
 (setq neo-theme 'ascii)
 
+;; key bindings for neotree
+;; make it behaving like vim's neotree
 (evil-define-key 'normal neotree-mode-map
   (kbd "RET") (neotree-make-executor
 		:file-fn 'neo-open-file
@@ -30,14 +33,20 @@
   (kbd "C-d") (neotree-make-executor
        :dir-fn 'neo-open-dired)
 
-  "g" 'neotree-refresh
+  "r" 'neotree-refresh
   "q" 'neotree-hide
+
   "p" 'neotree-previous-line
+  "k" 'neotree-previous-line
   (kbd "C-p") 'neotree-previous-line
+
   "n" 'neotree-next-line
+  "j" 'neotree-next-line
   (kbd "C-n") 'neotree-next-line
+
   "A" 'neotree-stretch-toggle
   "U" 'neotree-select-up-node
+  "u" 'neotree-select-up-node
   "D" 'neotree-select-down-node
   "H" 'neotree-hidden-file-toggle
   "S" 'neotree-select-previous-sibling-node
@@ -45,13 +54,22 @@
 
   "c" 'neotree-change-root
   "+" 'neotree-create-node
-  "r" 'neotree-rename-node
-  "d" 'neotree-delete-node
+  "ma" 'netoree-create-node
+  "mm" 'neotree-rename-node
+  "md" 'neotree-delete-node
   )
 
 (evil-leader/set-key
   "nn" 'neotree
-  "nc" 'neotree-toggle)
+  "nc" 'neotree-hide)
+
+;; unbind the default key for macro, use vim style instead.
+;; use [f3] to show or focus neotree and [f4] hide it.
+;;
+;; Note: I just setting it behaves like my vim settings, maybe it is
+;; not a good change.
+(global-set-key [f3] 'neotree)
+(global-set-key [f4] 'neotree-hide)
 
 
 (provide 'zcodes-neotree)
